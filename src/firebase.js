e8696daa15a1d88fdb4e3fc4ceea1b2633c9e08b
matwid03 <mwidawski13@gmail.com>
2025-02-07 20:52:48 +0100

    ApiKey

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyBzUJ4Iyb6knHLsE5aZqbPyZO7nUblwfMk",
+  authDomain: "todo-app-cac24.firebaseapp.com",
+  projectId: "todo-app-cac24",
+  storageBucket: "todo-app-cac24.firebasestorage.app",
+  messagingSenderId: "334575496468",
+  appId: "1:334575496468:web:f0b5f87a2d955110546601",
+  measurementId: "G-L9ZVHN9KBB"
 };
 
 const app = initializeApp(firebaseConfig);
